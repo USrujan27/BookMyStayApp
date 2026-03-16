b51413465cee0cb3c6f3ce27e0ad14c10c86ac5a
Srujan Uppalapu <srujanuppalapu27@gmail.com>
2026-03-16 09:25:57 +0530

    this is abt room types

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,12 +1,94 @@
+/**
+ * BookMyStayApp
+ *
+ * Demonstrates room modeling using abstraction, inheritance,
+ * polymorphism, and static availability variables.
+ *
+ * Book My Stay Application - Version 2.1
+ *
+ * @author Srujan Uppalapu
+ * @version 2.1
+ */
 
+/* Abstract Room Class */
+abstract class Room {
 
-public class BookMyStayApp{
-    public static void main(String []args){
-        System.out.println("=======================================");
-        System.out.println("   Welcome to Hotel Booking System");
-        System.out.println("   Application Version: v1.0");
-        System.out.println("=======================================");
-        System.out.println("System initialized successfully.");
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Price     : $" + price);
     }
 }
 
+/* Single Room Class */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 100);
+    }
+}
+
+/* Double Room Class */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 180);
+    }
+}
+
+/* Suite Room Class */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 300);
+    }
+}
+
+/* Main Application Class */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("        Book My Stay App");
+        System.out.println("           Version 2.1");
+        System.out.println("====================================");
+
+        // Creating room objects (Polymorphism)
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display Single Room
+        System.out.println("\nSingle Room Details:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailable);
+
+        // Display Double Room
+        System.out.println("\nDouble Room Details:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailable);
+
+        // Display Suite Room
+        System.out.println("\nSuite Room Details:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailable);
+
+        System.out.println("\nThank you for using Book My Stay App!");
+    }
+}
